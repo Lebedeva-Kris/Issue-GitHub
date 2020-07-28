89195113e18f5c01b59c7b92fbb0c73168d4bb33
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-28 17:59:37 +0300

    LastUpdateToLessComparator added

---
+++ src/main/java/ru/netology/domain/LastUpdateToLessComparator.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+import java.util.Comparator;
+import java.util.Date;
+
+public class LastUpdateToLessComparator implements Comparator<Issue> {
+    @Override
+    public int compare(Issue o1, Issue o2) {
+        Date first = o1.getLastUpdate().getTime();
+        Date second = o2.getLastUpdate().getTime();
+        return (first.before(second) ? 1 : (first.equals(second) ? 0 : -1));
+    }
+}
